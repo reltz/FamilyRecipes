427139a50581ea05b4af71e61fad42840553a6e2
Rodrigo Eltz <rodrigo_eltz@trendmicro.com>
2025-03-19 15:38:19 -0400

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Theme from "./Theme.tsx";
 import './i18n.ts'; // Import the i18n.ts file
 
 createRoot(document.getElementById("root")!).render(
   // <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       {" "}
       {/* Wrap your App with BrowserRouter */}
       <Theme>
         <App />
       </Theme>
-    </BrowserRouter>
+    </HashRouter>
   // </StrictMode>
 );
