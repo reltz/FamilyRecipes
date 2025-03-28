187ca64b7551dd8ecc30662fb042329365dd346b
Rodrigo Eltz <rpetrye@hotmail.com>
2025-03-28 11:03:39 -0400

    Clear state on logout- NEEDS TEST; (#10)
    * Clear state on logout

--- src/App.tsx
+++ src/App.tsx
@@ -21,11 +21,19 @@ function App() {
   const [cursor, setCursor] = useState<string | null>(null); // Store the cursor for pagination
   const [refreshTrigger, setRefreshTrigger] = useState(0); // Increment to trigger refresh
 
+  const clearState = () => {
+    setRecipes([]);
+    setCursor(null);
+    setHasMore(false);
+    setLoading(true);
+    setRefreshTrigger(0);
+    setIsAuthenticated(false);
+  }
 
   const onRecipeAdded = () => {
     setRecipes([]);
-    setCursor(null); // Reset cursor for pagination
-    setHasMore(false); // Reset pagination state
+    setCursor(null);
+    setHasMore(false);
     setRefreshTrigger((prev) => prev + 1); // Trigger re-fetch without clearing UI immediately
   };
 
@@ -33,14 +41,15 @@ function App() {
   const navigate = useNavigate();
 
   const handleLogout = () => {
-    setIsAuthenticated(false);
+    clearState();
     clearLocalToken();
-    onRecipeAdded();
+  
     navigate("/login");
   };
 
   const handleLogin = (status: boolean) => {
     setIsAuthenticated(status);
+    setRefreshTrigger((prev) => prev + 1);
     navigate("/");
   };
 
@@ -85,7 +94,7 @@ function App() {
     } else {
       setLoading(false);
     }
-  }, [refreshTrigger, isAuthenticated]); // Removed isAuthenticated as a dependency
+  }, [refreshTrigger, isAuthenticated]); 
 
   
   // Handle loading next page
